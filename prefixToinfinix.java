7d1d86464667c8ef59bb9e2ab9542d7e23310f27
shobankumar06 <shobankumar124@gmail.com>
2025-08-07 15:46:37 +0530

    added

---
+++ prefixToinfinix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Stack;
+public class Main
+{
+    public static String prefixToinfinix(String str){
+        String res="";
+        Stack<String> stack = new Stack<>();
+        for(int i=str.length()-1;i>=0;i--){
+            char x = str.charAt(i);
+            String newStr = String.valueOf(x);
+            if(Character.isLetterOrDigit(x)){
+                stack.push(newStr);
+            }
+            else{
+                String xx = stack.pop();
+                String yy = stack.pop();
+                res = "("+xx+newStr+yy+")";
+                stack.push(res);
+            }
+            
+        }
+        return stack.peek();
+    }
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		System.out.print(prefixToinfinix(str));
+	}
+}
